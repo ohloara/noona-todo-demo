100f39f32a07af99f9250bf0b70476b7360b1f7d
Udemy100 <srk03041@naver.com>
2024-05-25 05:09:44 +0900

    todo app v1

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,16 +1,20 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
 
-const TodoItem = () => {
+const TodoItem = ({item, toggleComplete, deleteTask}) => {
   return (
     <Row>
       <Col xs={12}>
-        <div className={`todo-item`}>
-          <div className="todo-content">밥먹기</div>
+        <div className={item.isComplete ? "todo-item complete":"todo-item"}>
+          <div className="todo-content">{item.task}</div>
 
           <div>
-            <button className="button-delete">삭제</button>
-            <button className="button-delete">끝남</button>
+            <button className="button-delete" onClick={()=>deleteTask(item._id)}>삭제</button>
+            <button className="button-delete"
+              onClick={()=>toggleComplete(item._id)}
+            >
+              {item.isComplete ? '안끝남':'끝남'}
+            </button>
           </div>
         </div>
       </Col>
